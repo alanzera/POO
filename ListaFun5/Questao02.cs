242d18d96777a8591883ca15b48feb440a5216cc
André Felipe <afsuper@hotmail.com>
2016-06-08 17:56:18 -0300

    Trocando ReadKey por ReadLine
    :>

--- ListaFun5/Questao02.cs
+++ ListaFun5/Questao02.cs
@@ -3,7 +3,7 @@ using System;
 public class Questao02 {
 	public static void Main (string[] args) {
 		Console.Write("Digite um número inteiro: ");
-		int n = int.Parse(Console.ReadKey());
+		int n = int.Parse(Console.ReadLine());
 
 		if (b % 5 == 0) Console.WriteLine("O número é múltiplo de 5");
 		else Console.WriteLine("O número não é múltiplo de 5");
